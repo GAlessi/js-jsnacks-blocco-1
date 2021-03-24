3006aa8a5c6c552a1c40264c8d29755e304d8a92
Gabriele Alessi <89archaon89@gmail.com>
2021-03-24 17:38:23 +0100

    Inserimento delle variabili all'interno delle funzioni

--- es24-03-2021/js/script.js
+++ es24-03-2021/js/script.js
@@ -1,43 +1,48 @@
-var student = {
-    'nome': 'Gabriele',
-    'cognome': 'Alessi',
-    'età': 18
-};
 
 
 function es1() {
+    var student = {
+        'nome': 'Gabriele',
+        'cognome': 'Alessi',
+        'età': 18
+    };
     for ( var key in student)
         console.log("Esercizio 1: ", student[key]);
 }
 
-var arrEs2 = [
-    {'nome': 'Gabriele', 'cognome': 'Alessi', 'età': 20},
-    {'nome': 'Alessando', 'cognome': 'Venti', 'età': 20},
-    {'nome': 'Lorenzo', 'cognome': 'Antonelli', 'età': 20}
-];
 
-// function es2() {
-//     for (var i = 0; i < arrEs2.length; i++) {
-//         var studente = arr[i];
-//         for (var key in studente) {
-//             var value = studente[key];
-//             console.log(key, value);
-//         }
-//     }
-// }
 function es2() {
+    var arrEs2 = [
+        {'nome': 'Gabriele', 'cognome': 'Alessi', 'età': 20},
+        {'nome': 'Alessando', 'cognome': 'Venti', 'età': 20},
+        {'nome': 'Lorenzo', 'cognome': 'Antonelli', 'età': 20}
+    ];
+    for (var i = 0; i < arrEs2.length; i++) {
+        var studente = arr[i];
+        for (var key in studente) {
+            var value = studente[key];
+            console.log(key, value);
+        }
+    }
+}
+function es2() {
+    var arrEs2 = [
+        {'nome': 'Gabriele', 'cognome': 'Alessi', 'età': 20},
+        {'nome': 'Alessando', 'cognome': 'Venti', 'età': 20},
+        {'nome': 'Lorenzo', 'cognome': 'Antonelli', 'età': 20}
+    ];
     for (var key in arrEs2) {
         console.log("Esercizio 2: ", arrEs2[key]['nome'] + ' ' + arrEs2[key]['cognome']);
     }
 }
 
-var arrEs3 = [
-    {'nome': 'Gabriele', 'cognome': 'Alessi', 'età': '31'},
-    {'nome': 'Alessando', 'cognome': 'Venti', 'età': '28'},
-    {'nome': 'Lorenzo', 'cognome': 'Antonelli', 'età': '30'}
-];
 
 function es3() {
+    var arrEs3 = [
+        {'nome': 'Gabriele', 'cognome': 'Alessi', 'età': '31'},
+        {'nome': 'Alessando', 'cognome': 'Venti', 'età': '28'},
+        {'nome': 'Lorenzo', 'cognome': 'Antonelli', 'età': '30'}
+    ];
     var newStudent = {
         "nome": prompt("nome"),
         "cognome": prompt("cognome"),
